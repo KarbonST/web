472bf71cb84aab3d95f5f766770cd53393f150cd
KarbonST <mikhailbykadorov@mail.ru>
2025-10-04 10:41:08 +0300

    feat(main.js): add light/dark themes toggle

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* Загрузка header и footer */
 async function loadPart(id, file) {
     try {
         const res = await fetch(file);
@@ -8,3 +9,29 @@ async function loadPart(id, file) {
 }
 loadPart("header", "public/header.html").then(r => console.log("Header загружен"));
 loadPart("footer", "public/footer.html").then(r => console.log("Footer загружен"));
+
+/* Переключение темы */
+const body = document.body;
+const toggle = document.getElementById("toggle");
+const icon = document.getElementById("theme-icon");
+
+// Првоерка сохраненной темы при загрузке страницы
+if (localStorage.getItem("theme") === "light"){
+  body.classList.remove("dark_theme");
+  icon.src = "public/images/moon.svg";
+} else {
+  body.classList.add("dark_theme");
+  icon.src = "public/images/sun.svg"
+}
+
+// При клике тема переключается
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark_theme");
+
+  // Сохранение темы в localStorage
+  const isDark = body.classList.contains("dark_theme");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+
+  // Смена иконки
+  icon.src = isDark ? "public/images/moon.svg" : "public/images/sun.svg";
+});
